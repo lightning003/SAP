a1ec057c9b7f5b8092034145e6e2501f36bb7247
Henal <merchanthenal@gmail.com>
2014-05-02 05:45:44 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SAP::Application.config.secret_key_base = 'e4874b0073d9db0c3bdf2de5d5118d4be0be1b1366e32c6ba1a44058dacfab243c2fbb00e633331fe3d316abfd257835bc72ad2f9971bd3cf076fcae3f870a14'
